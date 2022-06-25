90e1259d6b7be0f3ee20291f9994343e847fe840
Valentin <vellshow@bk.ru>
2022-06-25 18:42:16 +0300

    вызов имени

---
+++ test_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите свое имя:");
+String Username = Console.ReadLine();
+
+if(Username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же, МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(Username);
+}
